f52ad80c899bbf4ef1425478a3f09820a5c4905e
TakanaKonbu <takana.konbu150912@gmail.com>
2025-11-11 16:08:36 +0900

    テーマの修正

--- app/src/main/java/com/takanakonbu/academiamagica/ui/theme/Color.kt
+++ app/src/main/java/com/takanakonbu/academiamagica/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package com.takanakonbu.academiamagica.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkBrown = Color(0xFF1C1611)
+val Parchment = Color(0xFFF5EEDC)
+val DarkParchment = Color(0xFFDCD3B1)
+val Sepia = Color(0xFF704214)
+val Gold = Color(0xFFFFD700)
