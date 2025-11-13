b24d88d3cf4c4d409209c85658ec35610049a18b
TakanaKonbu <takana.konbu150912@gmail.com>
2025-11-13 13:40:09 +0900

    修正

--- app/src/main/java/com/takanakonbu/academiamagica/ui/screen/PrestigeScreen.kt
+++ app/src/main/java/com/takanakonbu/academiamagica/ui/screen/PrestigeScreen.kt
@@ -1,23 +1,15 @@
 package com.takanakonbu.academiamagica.ui.screen
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -29,7 +21,6 @@ import com.takanakonbu.academiamagica.ui.common.UpgradeItemCard
 import com.takanakonbu.academiamagica.ui.common.formatInflationNumber
 import com.takanakonbu.academiamagica.ui.viewmodel.GameViewModel
 import java.math.BigDecimal
-import java.math.RoundingMode
 
 private fun PrestigeSkillType.toJapanese(): String = when (this) {
     PrestigeSkillType.MANA_BOOST -> "✨ マナ生産量ボーナス"
